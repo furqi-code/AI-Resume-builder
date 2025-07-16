5491614df7eb04c025e083787cd70628c045c25b
Furqan <Git_practice@gmail.com>
2025-07-17 04:27:56 +0530

    printing actual html not the whole preview page

--- api/Routes/generatePdf.js
+++ api/Routes/generatePdf.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Router = express.Router() ;
 const {executeQuery} = require("../mySqldb/Query") ;
 // const html2pdf = require("html-pdf-node");
+const path = require("path") ;
 const ejs = require("ejs");
 const puppeteer = require("puppeteer") ;
 
@@ -17,8 +18,8 @@ Router.get("/", async function(req,res){
         let workExp = await executeQuery(`select * from job_history where resume_id = ? AND created_by = ?`, [resume_id, current_user]) ;
         let skills = await executeQuery(`select * from skills where resume_id = ? AND created_by = ?`, [resume_id, current_user]) ;
         let summary = await executeQuery(`select * from summary where resume_id = ? AND created_by = ?`, [resume_id, current_user]) ;
-
-        const htmlString = await ejs.renderFile("./views/preview.ejs", {
+        
+        const htmlString = await ejs.renderFile("./views/printPage.ejs", {
             isAuth,
             resume: resumeTitle[0],
             personalInfo: personalDetails[0],
@@ -35,8 +36,8 @@ Router.get("/", async function(req,res){
         await page.setContent(htmlString);
         await page.pdf({
             path: "resume.pdf",
-            // format: "A4",
-            width: "1140px",     
+            format: "A4",
+            // width: "1140px",     
             printBackground: true,
         });
 
